17a829a966c933994e79f6ffa6132bb42a9d2a19
leonardo.a.ribeiro.21@gmail.com <leonardo.a.ribeiro.21@gmail.com>
2020-10-19 17:17:49 -0300

    Exception handling - delete

---
+++ src/main/java/com/devsuperior/dspesquisa/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dspesquisa.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Ressource not found Id" + id);
+	}
+
+}
